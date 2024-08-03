910704f661956c498f4dcad2189a2c4ee79256c9
pyxn <51064667+pyxn@users.noreply.github.com>
2024-08-02 22:45:05 -0400

    export variables and make multiple levels

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -17,7 +17,7 @@ func _process(delta: float) -> void:
 
 func _on_body_entered(body: Node) -> void:
 	if "Goal" in body.get_groups():
-		complete_level()
+		complete_level(body.file_path)
 
 	if "Hazard" in body.get_groups():
 		crash_sequence()
@@ -26,6 +26,6 @@ func crash_sequence() -> void:
 	print("Kaboom")
 	get_tree().reload_current_scene()
 
-func complete_level () -> void:
+func complete_level (next_level_file: String) -> void:
 	print("Level complete.")
-	get_tree().quit()
+	get_tree().change_scene_to_file(next_level_file)
