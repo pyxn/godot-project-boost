3ee2f0c4c7526ec5362be0a678dfd101601a7872
pyxn <51064667+pyxn@users.noreply.github.com>
2024-08-02 23:01:42 -0400

    add tween to delay scene transition

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,6 +3,7 @@ extends RigidBody3D
 ## How much vertical force to apply when moving
 @export_range(750, 3000) var thrust: float = 1000.00
 @export var torque_thrust: float = 100.00
+var is_transitioning: bool = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -16,16 +17,26 @@ func _process(delta: float) -> void:
 		apply_torque(Vector3(0.0, 0.0, -torque_thrust * delta))
 
 func _on_body_entered(body: Node) -> void:
-	if "Goal" in body.get_groups():
-		complete_level(body.file_path)
+	if is_transitioning == false:
+		if "Goal" in body.get_groups():
+			complete_level(body.file_path)
 
-	if "Hazard" in body.get_groups():
-		crash_sequence()
+		if "Hazard" in body.get_groups():
+			crash_sequence()
 	
 func crash_sequence() -> void:
 	print("Kaboom")
-	get_tree().reload_current_scene()
+	set_process(false)
+	is_transitioning = true
+	var tween = create_tween()
+	tween.tween_interval(1.0)
+	tween.tween_callback(get_tree().reload_current_scene)
 
 func complete_level (next_level_file: String) -> void:
 	print("Level complete.")
-	get_tree().change_scene_to_file(next_level_file)
+	set_process(false)
+	is_transitioning = true
+	var tween = create_tween()
+	tween.tween_interval(1.0)
+	tween.tween_callback(get_tree().change_scene_to_file.bind(next_level_file))
+	
