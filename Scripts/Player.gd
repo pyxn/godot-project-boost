561d4969bdd93ef51c1209fd078e08b957407d14
pyxn <51064667+pyxn@users.noreply.github.com>
2024-08-03 12:00:33 -0400

    add particle effects on rocket boost

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -8,17 +8,33 @@ var is_transitioning: bool = false
 @onready var explosion_audio: AudioStreamPlayer = $ExplosionAudio
 @onready var success_audio: AudioStreamPlayer = $SuccessAudio
 @onready var rocket_audio: AudioStreamPlayer3D = $RocketAudio
+@onready var booster_particles: GPUParticles3D = $BoosterParticles
+@onready var right_booster_particles: GPUParticles3D = $RightBoosterParticles
+@onready var left_booster_particles: GPUParticles3D = $LeftBoosterParticles
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
 		apply_central_force(basis.y * delta * thrust)
+		booster_particles.emitting = true
+		if !rocket_audio.playing:
+			rocket_audio.play()
+	else:
+		booster_particles.emitting = false
+		rocket_audio.stop()
 		
 	if Input.is_action_pressed("rotate_left"):
 		apply_torque(Vector3(0.0, 0.0, torque_thrust * delta))
+		right_booster_particles.emitting = true
+	else:
+		right_booster_particles.emitting = false
 		
 	if Input.is_action_pressed("rotate_right"):
 		apply_torque(Vector3(0.0, 0.0, -torque_thrust * delta))
+		left_booster_particles.emitting = true
+	else:
+		left_booster_particles.emitting = false
+	
 
 func _on_body_entered(body: Node) -> void:
 	if is_transitioning == false:
@@ -43,6 +59,6 @@ func complete_level (next_level_file: String) -> void:
 	set_process(false)
 	is_transitioning = true
 	var tween = create_tween()
-	tween.tween_interval(1.0)
+	tween.tween_interval(2.414)
 	tween.tween_callback(get_tree().change_scene_to_file.bind(next_level_file))
 	
